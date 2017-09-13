0cc816d4cd26d7a6c9cf81b33db06665b8c184df
Bhavin Gandhi <bhavin7392@gmail.com>
2017-09-13 11:27:19 +0530

    Add validate_signature function We have to check if the given file is actual image file or not. To do this we will check the magic number of the file https://en.wikipedia.org/wiki/List_of_file_signatures This takes first 4 bytes of file as argument and return True or False.
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- image_downloader/main.py
+++ image_downloader/main.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 import requests
 
+def validate_signature(magic_numbers):
+    """Checks if the given bytes array of lenght 4 is matching with file signature of any image format
+    :param magic_numbers: byte array from file (first 4 bytes)
+    :returns: True if the given magic numbers are of image file"""
+
+    #PNG -> 89 50 4E 47
+    if magic_numbers == b'\x89\x50\x4E\x47':
+        return True
+    #JPG/JPEG -> FF D8 FF DB or FF D8 FF E0 or FF D8 FF E1
+    if magic_numbers in [b'\xFF\xD8\xFF\xDB', b'\xFF\xD8\xFF\xE0', b'\xFF\xD8\xFF\xE1']:
+        return True
+    return False
+
 def download_image(url):
     """Downloads the file from URL if it is a image
     :param url: string containing URL
@@ -10,7 +23,11 @@ def download_image(url):
         image = requests.get(url)
         if not image.ok:
             print("Unable to access the link");
-            return;
+            return
+        #check if it is a real image
+        if not validate_signature(image.content[:4]):
+            print("Image is not an real image")
+            return
         file_name = url.split("/")[-1]                  #last element would be file name
         with open(file_name, "wb") as image_file:
             image_file.write(image.content)
